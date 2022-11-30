f09859fcdaa5a85b0153874824806809a575d9f8
ElijahSum <85506313+ElijahSum@users.noreply.github.com>
2022-11-30 17:45:18 +0300

    second attempt

--- main.py
+++ main.py
@@ -1,77 +1,161 @@
+""" Advert class """
 import json
-import keyword
-from collections import abc
-
-json_dict = """{
-    "title": "Iphone 10",
-    "price": 100,
-    "location": {
-        "address": "город Москва, Лесная, 7",
-        "metro_stations": ["Белорусская"]
-        }
-}"""
+from keyword import iskeyword
+from collections import namedtuple
 
 
 class ColorizeMixin:
-    """ Добавляет цвет в print() """
-    repr_color_code = 32  # default colour, can be changed in Advert
+    """
+    Mixin for colorizing advertisement info
+    """
 
-    def __str__(self):
-        return f'\033[1;{self.repr_color_code}m {repr(self)}'
+    def __repr__(self):
+        color_code = self.repr_color_code
+        repr_str = super().__repr__()
+        return f"\033[1:{color_code}:48m{repr_str}\033[00m"
 
-
-class Unpacker:
-    """ Переделывает словарь в набор аттрибутов с соответствующим именем """
-
-    def __init__(self, mapping):
-        self._mapping = {}
-        for key, val in mapping.items():
-            if keyword.iskeyword(key):
-                self._mapping[key + '_'] = val
-            elif key == 'price':
-                self._mapping['_' + key] = val
-            else:
-                self._mapping[key] = val
-
-    def __getattr__(self, item):
-        if hasattr(self._mapping, item):
-            return getattr(self._mapping, item)
-        else:
-            if isinstance(self._mapping[item], abc.Mapping):
-                return Unpacker(self._mapping[item])  # рекурсивно вызываем метод
-            else:
-                return self._mapping[item]
+    def func(self):
+        """
+        Fictitious method to pass Pylint refactor problem
+        :return: None
+        """
 
 
-class Advert(ColorizeMixin, Unpacker):
-    """ Осуществляем взаимодействие с json файлами """
-    repr_color_code = 33
+class JSONParser:
+    """
+    Set fields to object from dict
+    """
+    def __call__(self, obj: object, dct: dict) -> None:
+        for key, val in dct.items():
+            if iskeyword(key):
+                key += '_'
 
-    def __init__(self, mapping):
-        super().__init__(mapping)  # dynamically gets attributes via Unpacker
-        self._price = self.price
+            if isinstance(val, dict):
+                key_type = namedtuple(f'{key}', val.keys())
+                setattr(obj, key, key_type(**val))
+                self.__call__(getattr(obj, key), val)
+            elif not hasattr(obj, key):
+                setattr(obj, key, val)
+
+    def func(self):
+        """
+        Fictitious method to pass Pylint refactor problem
+        :return: None
+        """
+
+
+class BaseAdvert:
+    """
+    Stores information about advertisement
+    """
+
+    def __init__(self, json_dict: dict):
+        dict_parser = JSONParser()
+        dict_parser(self, json_dict)
+        self._check_title()
+
+    def _check_title(self) -> None:
+        if not hasattr(self, 'title'):
+            raise ValueError("advertisement doesn't have field 'title'")
+
+    def _check_price(self, price=None) -> None:
+        price = self._price if price is None else price
+
+        if not isinstance(price, int):
+            raise ValueError(f"type(price) = {type(self.price)}, should be int")
+
+        if price < 0:
+            raise ValueError("must be >= 0")
 
     @property
-    def price(self):
-        try:
-            self._price
-        except:
-            return 0
-        else:
-            if self._price < 0:
-                raise ValueError('must be >= 0')
-            else:
-                return self._price
+    def price(self) -> int:
+        """
+        Price in the advertisement
+        :return price: int
+        """
+        return self._price
 
-    def __repr__(self) -> str:
-        return f'{self.title} | {self.price} ₽'
+    @price.setter
+    def price(self, value):
+        self._check_price(value)
+        self._price = value
 
-
-def main():
-    test = json.loads(json_dict)
-    test_class = Advert(test)
-    print(test_class.__repr__())
+    def __repr__(self):
+        return f"{getattr(self, 'title')} | {self.price} ₽"
 
 
-if __name__ == "__main__":
-    main()
+class Advert(ColorizeMixin, BaseAdvert):
+    """
+    Advertisement with colorized text
+    """
+
+    def __init__(self, json_dict: dict):
+        BaseAdvert.__init__(self, json_dict)
+        ColorizeMixin.__init__(self)
+
+    def func(self):
+        """
+        Fictitious method to pass Pylint refactor problem
+        :return: None
+        """
+
+
+if __name__ == '__main__':
+    LESSON_STR = """{
+        "title": "python",
+        "price": 0,
+        "location": {
+            "address": "город Москва, Лесная, 7",
+            "metro_stations": ["Белорусская"]
+        }
+    }"""
+    CORGI_STR = """{
+        "title": "Вельш-корги",
+        "price": 1000,
+        "class": "dogs",
+        "location": {
+            "address": "сельское поселение Ельдигинское, \
+                поселок санатория Тишково, 25"
+        },
+        "repr_color_code": 32
+    }"""
+    IPHONE_STR = """{
+        "title": "iPhone X",
+        "price": -100,
+        "location": {
+            "address": "город Самара, улица Мориса Тореза, 50",
+            "metro_stations": ["Спортивная", "Гагаринская"]
+        }
+    }"""
+
+    lesson = json.loads(LESSON_STR)
+    corgi = json.loads(CORGI_STR)
+    iphone = json.loads(IPHONE_STR)
+
+    lesson_ad = BaseAdvert(lesson)
+    corgi_ad = Advert(corgi)
+    iphone_ad = BaseAdvert(iphone)
+
+    print(lesson_ad)
+    print(lesson_ad.location.address)
+
+    print(corgi_ad)
+    print(corgi_ad.class_)
+
+    print(iphone_ad)
+    print(iphone_ad.price)
+
+    try:
+        iphone_ad.price = -1
+    except ValueError:
+        print("Negative price wasn't set!")
+
+    try:
+        BAD_IPHONE_STR = """{
+            "title": "iPhone X",
+            "price": -1
+        }"""
+        bad_iphone = json.loads(BAD_IPHONE_STR)
+        bad_iphone_ad = BaseAdvert(bad_iphone)
+    except ValueError as err:
+        print("Incorrect advertisement wasn't created!")
