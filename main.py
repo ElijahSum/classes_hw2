28039d9cfc7fc385ac30ba673031765996839105
ElijahSum <85506313+ElijahSum@users.noreply.github.com>
2022-12-02 14:12:27 +0300

    Final commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-import keyword
-from collections import abc
+from keyword import iskeyword
+from collections import namedtuple
 
 json_dict = """{
-    "title": "python",
-    "price": 0,
+    "title": "Iphone 10",
+    "price": 100,
     "location": {
         "address": "город Москва, Лесная, 7",
         "metro_stations": ["Белорусская"]
@@ -13,64 +13,83 @@ json_dict = """{
 
 
 class ColorizeMixin:
-    """ Добавляет цвет в print() """
-    repr_color_code = 32  # default colour, can be changed in Advert
+    """
+    Добавляет цвет в repr или str
+    """
+    repr_color_code = 33  # default colour, can be changed in Advert
 
-    def __str__(self):
-        return f'\033[1;{self.repr_color_code}m {repr(self)}'
+    def __repr__(self):
+        color_code = self.repr_color_code
+        repr_str = super().__repr__()
+        return f"\033[1:{color_code}:48m{repr_str}\033[00m"
 
 
 class Unpacker:
-    """ Переделывает словарь в набор аттрибутов с соответствующим именем """
+    """
+    Переделывает словарь в набор аттрибутов с соответствующим именем
+    """
 
-    def __init__(self, mapping):
-        self._mapping = {}
-        for key, val in mapping.items():
-            if keyword.iskeyword(key):
-                self._mapping[key + '_'] = val
-            elif key == 'price':
-                self._mapping['_' + key] = val
-            else:
-                self._mapping[key] = val
+    def __call__(self, obj: object, dct: dict) -> None:
+        for key, val in dct.items():
+            if iskeyword(key):
+                key += '_'
 
-    def __getattr__(self, item):
-        if hasattr(self._mapping, item):
-            return getattr(self._mapping, item)
-        else:
-            if isinstance(self._mapping[item], abc.Mapping):
-                return Unpacker(self._mapping[item])  # рекурсивно вызываем метод
-            else:
-                return self._mapping[item]
+            if isinstance(val, dict):
+                key_type = namedtuple(f'{key}', val.keys())
+                setattr(obj, key, key_type(**val))
+                self.__call__(getattr(obj, key), val)
+            elif not hasattr(obj, key):
+                setattr(obj, key, val)
 
 
-class Advert(ColorizeMixin, Unpacker):
-    """ Осуществляем взаимодействие с json файлами """
-    repr_color_code = 33
+class BaseAdvert:
+    """
+    Содержит всю информацию об объявлении
+    """
 
-    def __init__(self, mapping):
-        super().__init__(mapping)  # dynamically gets attributes via Unpacker
-        self._price = self.price
+    def __init__(self, mapping: dict):
+        dict_parser = Unpacker()
+        dict_parser(self, mapping)
+        self._check_title()
+
+    def _check_title(self) -> None:
+        if not hasattr(self, 'title'):
+            raise ValueError("advertisement doesn't have field 'title'")
+
+    def _check_price(self, price=None) -> None:
+        price = self._price if price is None else price
+
+        if not isinstance(price, int):
+            raise ValueError(f"Тип price - {type(self.price)}, ожидалось int")
+
+        if price < 0:
+            raise ValueError("must be >= 0")
 
     @property
-    def price(self):
-        try:
-            self._price
-        except KeyError:
-            return 0
-        else:
-            if self._price < 0:
-                raise ValueError('must be >= 0')
-            else:
-                return self._price
+    def price(self) -> int:
+        return self._price
 
-    def __repr__(self) -> str:
-        return f'{self.title} | {self.price} ₽'
+    @price.setter
+    def price(self, value):
+        self._check_price(value)
+        self._price = value
+
+    def __repr__(self):
+        return f"{getattr(self, 'title')} | {self.price} ₽"
+
+
+class Advert(ColorizeMixin, BaseAdvert):
+    """
+    Совмещает два родительских класса - делает цветной вывод
+    """
+    pass
 
 
 def main():
     test = json.loads(json_dict)
     test_class = Advert(test)
-    print(test_class.location.address)
+    print(test_class.__str__())
+    print(test_class.__dict__)
 
 
 if __name__ == "__main__":
